4147cc8decc21a059dfee6d739bce594454db912
Eliška Bartošová <bartosova.eli@gmail.com>
2020-10-22 00:35:20 +0200

    Hraje - pseudoprvky

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ console.log('funguju');
 
 let player = 'circle';
 
-const polickoElm = document.querySelector('.policko');
+const polickoElm = document.querySelectorAll('.policko');
 const hracElm = document.querySelector('.hrac');
 
 polickoElm.addEventListener('click', (event) => {
   if (player === 'circle') {
     event.target.className = 'board__field--circle';
-    hracElm.innerHTML = `<p class="hraje texty">hraje: </p>
-    <img src="images/cross.svg" alt="Křížek" />`;
+    hracElm.className = 'cross';
     player = 'cross';
     event.target.setAttribute('disabled', true);
   } else if (player === 'cross') {
     event.target.className = 'board__field--cross';
-    hracElm.innerHTML = `<p class="hraje texty">hraje: <span class="circle"></span></p>`;
+    hracElm.className = 'circle';
     player = 'circle';
     event.target.setAttribute('disabled', true);
   }
