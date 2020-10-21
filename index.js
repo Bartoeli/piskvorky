744fa3d66b46083ece4e55c725ebefd82f6ef531
Eliška Bartošová <bartosova.eli@gmail.com>
2020-10-22 00:58:51 +0200

    menší kolečko a křížek v poli

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const hracElm = document.querySelector('#player');
 
 poleElm.addEventListener('click', (event) => {
   if (player === 'circle') {
-    event.target.classList.Add('board__field--circle');
+    event.target.classList.add('board__field--circle');
     hracElm.className = 'cross';
     player = 'cross';
     event.target.setAttribute('disabled', true);
   } else if (player === 'cross') {
-    event.target.classList.Add('board__field--cross');
+    event.target.classList.add('board__field--cross');
     hracElm.className = 'circle';
     player = 'circle';
     event.target.setAttribute('disabled', true);
