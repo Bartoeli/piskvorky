0384c3cf93a1b557505710d993735f98478a9c17
Eliška Bartošová <bartosova.eli@gmail.com>
2020-10-22 20:56:04 +0200

    JS konečně funguje

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ console.log('funguju');
 let player = 'circle';
 
 const poleElm = document.querySelector('.pole');
-const hracElm = document.querySelector('#player');
+const hracElm = document.querySelector('.znak');
 
 poleElm.addEventListener('click', (event) => {
   if (player === 'circle') {
     event.target.classList.add('board__field--circle');
     event.target.disabled = true;
+    hracElm.src = 'images/cross.svg';
+    hracElm.alt = 'Křížek';
     player = 'cross';
-    hracElm.toggleClass('cross');
   } else if (player === 'cross') {
     event.target.classList.add('board__field--cross');
-    hracElm.className = 'circle';
-    player = 'circle';
     event.target.disabled = true;
+    hracElm.src = 'images/circle.svg';
+    hracElm.alt = 'Kolečko';
+    player = 'circle';
   }
 });
